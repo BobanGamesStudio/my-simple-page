0578406c8a4c93b406bf81193d8154c054494475
BobanGamesStudio <boban.skolimowski@gmail.com>
2025-05-31 19:07:54 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2em" }}>
+      <h1>Witaj na mojej stronie!</h1>
+      <p>To jest najprostsza strona React hostowana online.</p>
+    </div>
+  );
+}
+
+export default App;
